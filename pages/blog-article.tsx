e1ba374c57edb3138b0a6acac365c71869c17048
Lyumih <m.ivanov1225@gmail.com>
2020-05-13 14:27:13 +0300

    feat(project): add structure

---
+++ pages/blog-article.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Blog Article</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>Blog Article</p>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Ouruboros 2</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>Ouroboros 2</p>
+    </div>
+  );
+}
