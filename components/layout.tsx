ee18483bb15debc695beb26d0086c806b362a556
Lyumih <m.ivanov1225@gmail.com>
2020-05-13 15:14:05 +0300

    feat(project): add layout.js

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,12 +11,40 @@ export default function Layout({
   home?: boolean;
 }) {
   return (
-    <div className="container">
+    <div className="container page">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="og:title" content={siteTitle} />
       </Head>
-      <header>Header</header>
+      <header>
+        <div id="top-w">
+          <div id="top">
+            <a href="" className="logo">
+              Ouroboros
+            </a>
+            <div className="lang-w">
+              <a href="" className="lang-en lang-active">
+                en
+              </a>
+              <a href="" className="lang-ru">
+                ru
+              </a>
+              <a href="" className="lang-tr">
+                tr
+              </a>
+              <div className="clear"></div>
+            </div>
+            <div className="menu">
+              <a href="">About The Project</a>
+              <a href="">Road Map</a>
+              <a href="">What Makes Us Unique</a>
+              <a href="">Paramining</a>
+              <div className="clear"></div>
+            </div>
+            <div className="clear"></div>
+          </div>
+        </div>
+      </header>
       <main>{children}</main>
       <footer>footer</footer>
     </div>
